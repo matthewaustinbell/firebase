a65f5052fcfa4678f6c58b1d21ffe8743dfdc385
Matt Bell <matthewaustinbell@gmail.com>
2019-05-25 11:41:47 -0500

    added

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 
 import Auth from './components/auth/auth';
-import Birfday from './components/birfday/birfday';
+
 import MyNavbar from './components/MyNavbar/MyNavbar';
 
 import authData from './helpers/data/authData';
@@ -15,7 +15,6 @@ const init = () => {
   MyNavbar.navbarEvents();
   authData.checkLoginStatus();
   Auth.authStringBuilder();
-  Birfday.birfdayStringBuilder();
 };
 
 init();
