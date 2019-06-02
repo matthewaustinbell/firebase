73c2dca3e19f94d2e459622c509cfae2c929d855
Matt Bell <matthewaustinbell@gmail.com>
2019-06-02 13:43:15 -0500

    working

--- src/javascripts/helpers/smash.js
+++ src/javascripts/helpers/smash.js
@@ -1,12 +1,10 @@
-
-
 const friendRsvps = (friends, rsvps) => friends.map((friend) => {
   console.error(rsvps);
   const f = friend;
   const rsvp = rsvps.find(r => r.friendId === f.id);
   if (rsvp) {
-    f.rsvpId = 'rsvp.id';
-    f.statusId = 'rsvp.statusId';
+    f.rsvpId = rsvp.id;
+    f.statusId = rsvp.statusId;
   }
   return f;
 });
